0dcf13f97273d8cdb84a87e89d736b28f26396ab
sereginski <relict1900@bk.ru>
2022-11-21 00:07:56 +0300

    Name1

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Юлиана")
+{
+    Console.WriteLine("Ура, это же моя любимая жена!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
